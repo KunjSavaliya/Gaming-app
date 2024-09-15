cb1f02333f8fde4321a3a72d0c6b9ae7bdc55c9f
KunjSavaliya <kunj.bvminfotech@gmail.com>
2024-09-15 19:49:43 +0530

    sidebar chnage

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -11,6 +11,9 @@ function Header() {
 
   const handleNavigation = (path) => {
     router.push(path);
+    if (isSidebarOpen === true) {
+      setIsSidebarOpen(false);
+    }
   };
 
   const toggleSidebar = () => {
@@ -20,9 +23,9 @@ function Header() {
   return (
     <header>
       {/* Top Sidebar */}
-      <div className={`fixed top-0 z-10 left-0 w-full h-full bg-gray-800 text-white transition-transform ${isSidebarOpen ? 'translate-y-0' : '-translate-y-full'} lg:hidden flex flex-col`}>
+      <div className={`fixed top-0 z-30 left-0 w-full h-full bg-gray-800 text-white transition-transform ${isSidebarOpen ? 'translate-y-0' : '-translate-y-full'} lg:hidden flex flex-col`}>
         <div className="absolute top-4 right-4">
-          <HiX className="text-2xl cursor-pointer" onClick={toggleSidebar} />
+          <HiX className="text-2xl cursor-pointer text-[#ff56f8]" onClick={toggleSidebar} />
         </div>
         <div className="flex flex-col items-center justify-center mt-10 space-y-3 cursor-pointer">
           <p className="py-2 cursor-pointer hover:bg-[#ff56f8] p-1 rounded-lg hover:text-white transition-all duration-300" onClick={() => handleNavigation('/Pages/Action')}>Action</p>
@@ -37,7 +40,7 @@ function Header() {
       <div className='flex items-center justify-between lg:justify-center lg:gap-8 w-[100%] lg:w-full p-5 text-black lg:flex-row'>
         {/* Sidebar Toggle Button */}
         <div className='top-5 left-5 lg:hidden'>
-          <HiMenu className='text-2xl cursor-pointer' onClick={toggleSidebar} />
+          <HiMenu className='text-2xl cursor-pointer text-[#ff56f8]' onClick={toggleSidebar} />
         </div>
 
         {/* Logo */}
@@ -62,7 +65,7 @@ function Header() {
             <p className='hover:bg-[#ff56f8] p-1 rounded-lg hover:text-white transition-all duration-300' onClick={() => handleNavigation('/Pages/Puzzle')}>Puzzle</p>
           </div>
           <div className='flex items-center justify-center bg-[#ff56f8] rounded-full p-2 cursor-pointer lg:mt-[-2px] mt-[-15px]'>
-            <PiMagnifyingGlass className='text-xl font-extrabold text-white' />
+            <PiMagnifyingGlass className='text-xl font-extrabold text-white ' />
           </div>
         </div>
       </div>
