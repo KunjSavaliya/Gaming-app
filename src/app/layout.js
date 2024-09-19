17906461759243c1ba78cfa42dca8b348ef4d338
KunjSavaliya <kunj.bvminfotech@gmail.com>
2024-09-19 17:03:29 +0530

    Favicon.ico

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,25 +2,29 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import {SearchProvider} from './Components/SerchContext.jsx'
+import { SearchProvider } from './Components/SerchContext.jsx';
+import Head from 'next/head'; // Import the Head component
+
 export const metadata = {
-  title: "Gaming App",
+  title: "onlinegameforyou!",
   description: "Your ultimate gaming app experience with amazing features and gameplay!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <SearchProvider>
-    <html lang="en">
-      <body
-      >
-        <Header />
-        <div className=''>
-        {children}
-        </div>
-        <Footer />
-      </body>
-    </html>
+      <html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.ico" /> {/* Link to your favicon */}
+          <title>{metadata.title}</title>
+          <meta name="description" content={metadata.description} />
+        </Head>
+        <body>
+          <Header />
+          <div>{children}</div>
+          <Footer />
+        </body>
+      </html>
     </SearchProvider>
   );
 }
